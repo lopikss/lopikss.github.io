c0f69c88cfec11fdfd2d447b06a0a85ccfb6317f
lopikss <119501417+lopikss@users.noreply.github.com>
2023-02-07 17:48:29 +0100

    Update position.js

--- host.embed/position.js
+++ host.embed/position.js
@@ -1,14 +1,3 @@
-function launchIntoFullscreen(element) {
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-      } else if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-      }
-    }
     var offsetX = -3;
     var offsetY = -5;
     var fullscreenButton = document.getElementById("fullscreen-button");
