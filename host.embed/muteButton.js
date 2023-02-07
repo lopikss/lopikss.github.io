d84c44878e6a4deea0bf59ca44d89dfdcac6ca90
lopikss <119501417+lopikss@users.noreply.github.com>
2023-02-07 17:48:45 +0100

    Delete muteButton.js

--- host.embed/muteButton.js
+++
@@ -1,8 +0,0 @@
- function toggleMute() {
-    var flashContent = document.getElementById("flash-content");
-    if (flashContent.muted) {
-      flashContent.muted = false;
-    } else {
-      flashContent.muted = true;
-    }
-  }
